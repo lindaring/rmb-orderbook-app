934980f4a4fa8ad062dff3f00731a7b98d712f0e
Lindaring <s1m3l4n3@gmail.com>
2023-01-17 09:18:26 +0200

    Project initialization

---
+++ src/test/java/za/co/rmb/orderbook/OrderbookApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.rmb.orderbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderbookApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
